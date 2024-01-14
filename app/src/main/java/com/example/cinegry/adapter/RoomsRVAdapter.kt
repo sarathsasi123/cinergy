d44298a8881014fb98f6b0a4d3368dd0cc43a325
Sarath <sarath.sasi@viewway.net>
2024-01-14 17:33:20 +0530

    Placeholder updated

--- app/src/main/java/com/example/cinegry/adapter/RoomsRVAdapter.kt
+++ app/src/main/java/com/example/cinegry/adapter/RoomsRVAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.cinegry.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cinegry.R
